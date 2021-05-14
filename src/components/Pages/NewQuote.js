be6024bb353646b21e6e333d972ecd79828cb9e3
Sir-Unkie <unknown920@gmail.com>
2021-05-14 17:03:37 +0300

    Prompt added (prevent accidental form data removal)

--- src/components/Pages/NewQuote.js
+++ src/components/Pages/NewQuote.js
@@ -1,8 +1,11 @@
 import QuoteForm from '../quotes/QuoteForm';
+import { useHistory } from 'react-router-dom';
 
 const NewQuote = () => {
+  const history = useHistory();
   const addQuoteHandler = quoteDAta => {
     console.log(quoteDAta);
+    history.push('/quotes');
   };
   return <QuoteForm onAddQuote={addQuoteHandler}></QuoteForm>;
 };
